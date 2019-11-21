c4a64f1982550124a274bab1a6ee19ac3ad7d070
Maykon Oliveira <maykonoliveira850@gmail.com>
2019-11-21 18:30:59 -0300

    Add leaflet

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Map, tileLayer, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements AfterViewInit {
 
-  constructor() {}
+  constructor(public http: HttpClient, public plt: Platform, public router: Router) { }
+
+  ngAfterViewInit() {
+    this.plt.ready().then(() => {
+      this.http.get('https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev')
+        .subscribe(restaurants => this.initMap(restaurants));
+    });
+  }
+
+  initMap(restaurants) {
+    const map = new Map('map').setView([-5.9511452, -35.2697794], 13);
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // const customMarkerIcon = icon({
+    //   iconUrl: 'assets/images/custom-marker-icon.png',
+    //   iconSize: [64, 64],
+    //   popupAnchor: [0, -20]
+    // });
+
+    // restaurants.forEach((restaurant) => {
+    //   marker([restaurant.position.lat, restaurant.position.lgn], { icon: customMarkerIcon })
+    //     .bindPopup(`<b>${restaurant.title}</b>`, { autoClose: false })
+    //     .on('click', () => this.router.navigateByUrl('/restaurant'))
+    //     .addTo(map).openPopup();
+    // });
+  }
 
 }
